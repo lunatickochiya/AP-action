4f8857743047e2dfa2886abd01fe2f9f646f0b6b
lunatickochiya <125438787@qq.com>
2026-08-23 17:20:24 +0800

    luci-app-openvpn-server-client:compatible with 25.12 apk

--- package/kochiya/luci-app-openvpn-server-client/root/etc/openvpncert.sh
+++ package/kochiya/luci-app-openvpn-server-client/root/etc/openvpncert.sh
@@ -1,34 +1,52 @@
 #!/bin/sh
 
 
-function rand_str() {
+rand_str() {
 	(base64 /dev/urandom | tr -dc 'A-Za-z' | head -c $1) 2>/dev/null
 }
 
-function rand_str_upper() {
+rand_str_upper() {
 	(rand_str $1 | tr 'a-z' 'A-Z') 2>/dev/null
 }
 
-function rand_str_lower() {
+rand_str_lower() {
 	(rand_str $1 | tr 'A-Z' 'a-z') 2>/dev/null
 }
 
-function rand_easy_rsa_vars() {
+set_easy_rsa_var() {
+	local name="$1"
+	local value="$2"
+
+	if grep -q "^[[:space:]]*set_var[[:space:]]\+$name[[:space:]]" /etc/easy-rsa/vars; then
+		sed -i "s|^[[:space:]]*set_var[[:space:]]\+$name[[:space:]].*$|set_var $name\t$value|" /etc/easy-rsa/vars
+	else
+		printf 'set_var %s\t%s\n' "$name" "$value" >> /etc/easy-rsa/vars
+	fi
+}
+
+init_easy_rsa_vars() {
+	touch /etc/easy-rsa/vars
+	set_easy_rsa_var EASYRSA_OPENSSL '"openssl"'
+	set_easy_rsa_var EASYRSA_KEY_SIZE '2048'
+	set_easy_rsa_var EASYRSA_CA_EXPIRE '3650'
+	set_easy_rsa_var EASYRSA_CERT_EXPIRE '3650'
+}
+
+rand_easy_rsa_vars() {
 	local KEY_PROVINCE="$(rand_str_upper 6)"
 	local KEY_CITY="$(rand_str 8)"
 	local KEY_ORG="$(rand_str 8)"
 	local KEY_EMAIL="$(rand_str_lower 8)@$(rand_str_lower 4).$(rand_str_lower 3)"
 	local KEY_OU="$(rand_str 8)"
-	sed -i \
-		-e "s/^[[:space:]]*set_var[[:space:]]\+EASYRSA_REQ_COUNTRY[[:space:]]\+\".*\"$/set_var EASYRSA_REQ_COUNTRY\t\"$KEY_PROVINCE\"/" \
-		-e "s/^[[:space:]]*set_var[[:space:]]\+EASYRSA_REQ_PROVINCE[[:space:]]\+\".*\"$/set_var EASYRSA_REQ_PROVINCE\t\"$KEY_CITY\"/" \
-		-e "s/^[[:space:]]*set_var[[:space:]]\+EASYRSA_REQ_CITY[[:space:]]\+\".*\"$/set_var EASYRSA_REQ_CITY\t\"$KEY_ORG\"/" \
-		-e "s/^[[:space:]]*set_var[[:space:]]\+EASYRSA_REQ_ORG[[:space:]]\+\".*\"$/set_var EASYRSA_REQ_ORG\t\"$KEY_ORG\"/" \
-		-e "s/^[[:space:]]*set_var[[:space:]]\+EASYRSA_REQ_EMAIL[[:space:]]\+\".*\"$/set_var EASYRSA_REQ_EMAIL\t\"$KEY_EMAIL\"/" \
-		-e "s/^[[:space:]]*set_var[[:space:]]\+EASYRSA_REQ_OU[[:space:]]\+\".*\"$/set_var EASYRSA_REQ_OU\t\"$KEY_OU\"/" \
-		/etc/easy-rsa/vars
+	set_easy_rsa_var EASYRSA_REQ_COUNTRY "\"$KEY_PROVINCE\""
+	set_easy_rsa_var EASYRSA_REQ_PROVINCE "\"$KEY_CITY\""
+	set_easy_rsa_var EASYRSA_REQ_CITY "\"$KEY_ORG\""
+	set_easy_rsa_var EASYRSA_REQ_ORG "\"$KEY_ORG\""
+	set_easy_rsa_var EASYRSA_REQ_EMAIL "\"$KEY_EMAIL\""
+	set_easy_rsa_var EASYRSA_REQ_OU "\"$KEY_OU\""
 }
 
+init_easy_rsa_vars
 rand_easy_rsa_vars
 
 
